b3ce80a543e7eddd5646a08002a8f6c59ca050bc
Christian Papauschek <christian@journiapp.com>
2022-12-02 16:59:31 +0100

    advent 1

--- src/main/scala/Advent1.scala
+++ src/main/scala/Advent1.scala
@@ -3,5 +3,9 @@ import java.nio.file.{Files, Path}
 object Advent1:
 
   def main(args: Array[String]): Unit =
-    println("hello world")
+    val input = Files.readString(Path.of("inputs/input1.txt")).trim
+    val elfs = input.split("\n\n").toSeq
+    val elfsWithCalories = elfs.map(_.linesIterator.map(_.toInt).sum)
+    val topThreeSum = elfsWithCalories.sorted.reverse.take(3).sum
+    println(topThreeSum)
 
