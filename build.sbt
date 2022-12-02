9dc0ade0379f38dcc0a5666003aeed9b1b9354ed
Christian Papauschek <christian@journiapp.com>
2022-12-02 16:43:15 +0100

    initial

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "advent2022"
+
+version := "0.1"
+
+scalaVersion := "3.2.1"
---
+++ src/main/scala/Advent1.scala
@@ -0,0 +1,7 @@
+import java.nio.file.{Files, Path}
+
+object Advent1:
+
+  def main(args: Array[String]): Unit =
+    println("hello world")
+
